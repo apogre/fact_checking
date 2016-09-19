516cf7281d9bf3d9f936acb04997a26e3826a8bc
anish pradhan <anish pradhan>
2016-09-19 11:39:30 -0700

    algo

--- dbpedia.py
+++ dbpedia.py
@@ -1,6 +1,6 @@
 import sparql
 import csv
-# e1 = select distinct ?c where{?x rdfs:label "Beckham"@en . ?x rdf:type/rdfs:subClassOf* ?c . }
+# e1 = select distinct ?c where{?x rdfs:label "Beckham"@en . }
 # SELECT ?type ?superType WHERE 
 # { 
 #   # give me ?type of the resource
@@ -11,43 +11,47 @@ import csv
 #    ?type rdfs:subClassOf ?superType .
 #   }
 # }
-labels =['"Thierry Henry"@en','"Arsenal F.C."@en']
+
+labels =['"David Beckham"@en','"Victoria Beckham"@en']
 entities = {} 
 for i,label in enumerate(labels):
-	q = ('select distinct ?c where{?x rdfs:label'+ label +' . ?x rdf:type/rdfs:subClassOf* ?c . }')
+	q = ('select distinct ?x where{?x rdfs:label'+ label +' }')
 	result = sparql.query('http://dbpedia.org/sparql', q)
-	# print result	
-	# str('entity'+i) = []
-	my_list = []
+	types = {}
 	for row in result:
-	# 	# print row
 		values = sparql.unpack_row(row)
-		my_list.append(values[0])
-	# 	print values
-	entities[i]= my_list
-	# print entities	
-	# print "==========="
+		if not 'Category:' in values[0]:
+			my_list = []
+			q1=('SELECT distinct ?type ?superType WHERE  { <'+str(values[0]) + '> rdf:type ?type . optional { ?type rdfs:subClassOf ?superType}}')
+			result1 = sparql.query('http://dbpedia.org/sparql', q1)
+			for row1 in result1:
+				values1 = sparql.unpack_row(row1)
+				my_list.append(values1)
+			types[values[0]]= my_list
+	entities[i] = types	
+print "==========="
+print entities
 
 # relation=[]
-with open('mydata_rev_new.csv','wb') as f:
-	writer = csv.writer(f)
-	for i,item1 in enumerate(entities[1]):
-		print i	
-		for j,item2 in enumerate(entities[0]):
-			q1=('SELECT ?r WHERE { ?x1 rdfs:label <'+str(item1) + '> . ?x2 rdfs:label <' + item2 +'> . ?x1 ?r ?x2}')
-			# print q1
-			try:
-				result1 = sparql.query('http://dbpedia.org/sparql', q1)
-				for row1 in result1:
-					values1 = sparql.unpack_row(row1)
-					# print values1
-					if values1:
-						print j
-						print([str(item1),str(values1[0]),str(item2)])
-						writer.writerow([str(item1),str(values1[0]),str(item2)])	
-					# relation.append(values1[0])
-			except:
-				pass
+# with open('mydata_type.csv','wb') as f:
+	# writer = csv.writer(f)
+	# for i,item1 in enumerate(entities[1]):
+		# print i	
+		# for j,item2 in enumerate(entities[0]):
+			# q1=('SELECT ?type ?superType WHERE  { <'+str(item1) + '> rdf:type ?type . optional { ?type rdfs:subClassOf ?superType}')
+# 			# print q1
+# 			try:
+# 				result1 = sparql.query('http://dbpedia.org/sparql', q1)
+# 				for row1 in result1:
+# 					values1 = sparql.unpack_row(row1)
+# 					# print values1
+# 					if values1:
+# 						print j
+# 						print([str(item1),str(values1[0]),str(item2)])
+# 						writer.writerow([str(item1),str(values1[0]),str(item2)])	
+# 					# relation.append(values1[0])
+# 			except:
+# 				pass
 
 # with open('mydata_1.csv','wb') as f:
 # 	writer = csv.writer(f)

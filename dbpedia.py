cb5a36e950bed8a91c9c627901fba1bf6460cdf5
anish pradhan <mail.anishpradhan@gmail.com>
2016-09-19 12:58:23 -0700

    added

--- dbpedia.py
+++ dbpedia.py
@@ -1,5 +1,9 @@
 import sparql
 import csv
+import subprocess
+import csv
+import os
+
 # e1 = select distinct ?c where{?x rdfs:label "Beckham"@en . }
 # SELECT ?type ?superType WHERE 
 # { 
@@ -11,6 +15,39 @@ import csv
 #    ?type rdfs:subClassOf ?superType .
 #   }
 # }
+os.chdir('../models/syntaxnet/')
+
+subprocess.call(['echo "President lives in White House." | syntaxnet/demo.sh > output.txt'],shell=True)
+
+filename='output.txt'
+
+def readata(filename):
+    file=open(filename,'r')
+    lines=file.readlines()
+    lines=lines[:-1]
+    data=csv.reader(lines,delimiter='\t')
+#make a list of lists
+    lol=list(data)
+    return  lol
+#find the verb which is ROOT of the sentence
+
+lists=readata(filename)
+NNP_list = {}
+for i,sublist in enumerate(lists):
+	# print sublist
+	if sublist[4]=='NNP':
+		word = sublist[1]
+		NNP_list[i] = word
+	elif sublist[7]=='ROOT':
+		predicate = sublist[1]
+
+print NNP_list
+print predicate
+
+flag = 0
+for k,v in NNP_list.iteritems():
+	if k != 0 or flag==0:
+		print k
 
 labels =['"David Beckham"@en','"Victoria Beckham"@en']
 entities = {} 
@@ -21,6 +58,7 @@ for i,label in enumerate(labels):
 	for row in result:
 		values = sparql.unpack_row(row)
 		if not 'Category:' in values[0]:
+			print values[0]
 			my_list = []
 			q1=('SELECT distinct ?type ?superType WHERE  { <'+str(values[0]) + '> rdf:type ?type . optional { ?type rdfs:subClassOf ?superType}}')
 			result1 = sparql.query('http://dbpedia.org/sparql', q1)
@@ -30,7 +68,7 @@ for i,label in enumerate(labels):
 			types[values[0]]= my_list
 	entities[i] = types	
 print "==========="
-print entities
+# print entities
 
 # relation=[]
 # with open('mydata_type.csv','wb') as f:
